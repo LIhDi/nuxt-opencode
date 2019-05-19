ed34a7038d9aa997a7ae5370a1882acc17006fd2
Lih <aline.diogo@crea-rj.org.br>
2019-05-19 18:15:20 -0300

    fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,37 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+  <v-app>
+    <v-flex id="opencode"></v-flex>
+      <v-flex xs10 offset-xs1>
+        <v-card id="container-secundario">
+          <v-toolbar height="50px" card prominent>
+            <Menu></Menu>
+          </v-toolbar>
+          <v-divider></v-divider>
+          <v-card-text style="height: 600px;"></v-card-text>
+        </v-card>
+      </v-flex>
+  </v-app>
 </template>
-
 <script>
+import Menu from '../components/toolbar/Menu.vue'
 export default {
+  components: {
+    Menu
+  },
   data() {
     return {
     }
   }
 }
 </script>
-
+<style>
+    #opencode{
+    width:1605px;
+    height: 250px;
+    background: url(../static/teste65.jpg);
+    background-size:cover;
+  }
+  #container-secundario{
+    margin-top: -64px;
+  }
+</style>
