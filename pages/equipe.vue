da4866bbdd44253d070a080af6dfd2fff634079b
Lih <aline.diogo@crea-rj.org.br>
2019-07-13 22:39:15 -0300

    equipe

---
+++ pages/equipe.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <v-container>
+    <v-layout pa-4 row wrap>
+        <v-flex xs12 sm12 md2 >
+          <v-avatar size="120">
+            <img src="../assets/images/team/lihdi.jpeg" alt="lihdi">
+          </v-avatar>
+        </v-flex>
+        <v-flex xs12 sm12 md3 >
+          <div class="body-1">
+          <span class="body-2">Aline Diogo - LihDi</span><br/>
+          <v-icon>mdi-briefcase</v-icon><span>FullStack Developer</span><br/>
+          <v-icon>mdi-map-marker</v-icon><span>Portugal</span><br/>
+          <v-icon>mdi-earth</v-icon><span>Português - English</span><br/>
+          <v-btn icon small outline class="ml-0 white pink--text"><v-icon>mdi-github-circle</v-icon></v-btn>
+          <v-btn icon small outline class="ml-0 white pink--text"><v-icon>mdi-linkedin</v-icon></v-btn>
+          </div>
+        </v-flex>
+        <v-flex xs12 sm12 md5>
+          <span class="body-2 grey--text">Sou uma pessoa apaixonada por tecnologia, busco sempre a melhoria contínua, 
+             para mim é um privilégio contribuir com a comunidade...
+          </span>
+        </v-flex>
+    </v-layout>
+    <v-layout pa-4 row wrap>
+        <v-flex xs12 sm12 md2 >
+          <v-avatar class="ma-3" size="120">
+          <img src="../assets/images/team/btm.jpeg" alt="lihdi">
+        </v-avatar>
+        </v-flex>
+        <v-flex xs12 sm12 md3 >
+          <span class="body-2">Sou uma pessoa apaixonada por tecnologia, busco sempre a melhoria contínua, 
+             para mim é um privilégio contribuir com a comunidade.
+          </span>
+        </v-flex>
+        <v-flex xs12 sm12 md4 >
+          <span>Bernard Mendes - Btm</span><br/>
+          <span>FullStack Developer</span><br/>
+          <span>Brasil</span>
+        </v-flex>
+    </v-layout>
+    <v-layout pa-4 row wrap class="ma-1">
+        <v-flex xs12 sm12 md2 >
+          <v-avatar class="ma-3" size="120">
+          <img src="../assets/images/team/nomar.jpeg" alt="lihdi">
+        </v-avatar>
+        </v-flex>
+        <v-flex xs12 sm12 md3 >
+          <span class="body-2">Sou uma pessoa apaixonada por tecnologia, busco sempre a melhoria contínua, 
+             para mim é um privilégio contribuir com a comunidade.
+          </span>
+        </v-flex>
+        <v-flex xs12 sm12 md4 >
+          <span>Ramon Paiva - Nomar</span><br/>
+          <span>Software Engineer</span><br/>
+          <span>Brasil</span><br/>
+          <span>Ramon Paiva - Nomar</span><br/>
+        </v-flex>
+    </v-layout>
+    </v-container>
+  </div>
+</template>
+<script>
+</script>
+<style scoped>
+.v-icon{
+  font-size: 15px;
+}
+
+</style>
+
+
