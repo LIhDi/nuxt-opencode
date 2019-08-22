341d5f05f4994edae38b8fd24a40f086aa503616
Lih <alinecsdiogo@gmail.com>
2019-08-21 21:56:06 -0300

    corrige barra de pesquisa

--- components/Sections/BlogSection.vue
+++ components/Sections/BlogSection.vue
@@ -1,21 +1,6 @@
 <template>
   <section>
     <v-container fluid >
-      <v-layout row wrap >
-        <v-flex xs12 sm12 md12 class="pa-3 grey">
-          <v-layout justify-center row wrap>
-          <v-flex xs6 sm6 md6 class="transparent">
-              <v-input
-              class="white"
-              rounded
-                hide-details="true"
-                append-icon="search"
-                @click:append="appendIconCallback"
-                @click:prepend="prependIconCallback">Tutoriais</v-input>
-          </v-flex>
-          </v-layout>
-        </v-flex>
-      </v-layout>
       <v-layout row wrap>
         <v-flex xs12 sm12 md7 class="pa-3">
           <blog-card :altura="420" :blog="blogs[0]" />
